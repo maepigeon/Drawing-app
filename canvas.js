07a7b839ed98cbdb4b609e30f051d38f69977f24
Michael Pigeon <michaellpigeon@gmail.com>
2022-02-22 17:12:55 -0500

    added a readme and redo functionality

--- canvas.js
+++ canvas.js
@@ -1,36 +1,45 @@
-// drawing functionality starting point from:
+// basic drawing functionality starting point from:
 // https://www.youtube.com/watch?v=3GqUM4mEYKA
 
 // added:
 //  (1) undo
 //  (2) color selection
 //  (3) eraser
+//  (4) redo
 
 // todo:
 // pressure
 // catmull-rom based smoothing
 // size/color selectors
 // syncing using websockets and a webserver
+// gui
 
 
-let layers = []
-let undoHistory = []
-let numberOfUndos = 0
+let layers = [];
+let history = [];
+let undoHistory = [];
 
-let brush = {size: 10, color: "red"}
+let brush = {size: 10, color: "red"};
 
-let eraser = false
+let eraser = false;
 
 // saves the state of a layer to the history stack
 function saveLayer(layeridx, operationName) {
-    numberOfUndos = 0;
     var image = layers[0].toDataURL("image/png");
-    undoHistory.push({operation: operationName, layer: layeridx, url: image});
+    undoHistory = []
+    history.push({operation: operationName, layer: layeridx, url: image});
 }
 
-// restores the state of the canvas since the last modifying operation
-function undo() {
-    var lastCanvasState = undoHistory.pop().url;
+// undos the last undo
+function redo() {
+    if (undoHistory.length === 0) {
+        alert("nothing left to be redone");
+        return;
+    }
+    var lastState = undoHistory.pop();
+    history.push(lastState); // backup this redo so it can be undone
+
+    var lastCanvasState = lastState.url; // url to the stored image
     var canvas = layers[0];
     var ctx = canvas.getContext("2d");
     ctx.globalCompositeOperation = "source-over";
@@ -43,38 +52,68 @@ function undo() {
     img.onload = function(){
         ctx.drawImage(img,0,0); 
     };
-    numberOfUndos++;
+}
+
+
+// restores the state of the canvas since the last modifying operation
+function undo() {
+    if (history.length === 0) {
+        alert("nothing left to undo.");
+        return;
+    }
+    var lastState = history.pop();
+    undoHistory.push(lastState); // backup this undo so it can be redone
+
+    var lastCanvasState = history[history.length - 1].url; // get url to the stored image
+    var canvas = layers[0];
+    var ctx = canvas.getContext("2d");
+    ctx.globalCompositeOperation = "source-over";
+    var url = lastCanvasState;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+
+    var img = new Image; // load saved image of previous layer state
+    img.src = url;
+    img.onload = function(){
+        ctx.drawImage(img,0,0); 
+    };
 }
 
 window.addEventListener("load", () => {
     layers[0] = document.querySelector("#canvas");
     var canvas = layers[0]
     const ctx = canvas.getContext("2d");
+    saveLayer(0, "brush");
 
-    // Resizing
-    canvas.height = 1000;
-    canvas.width = 1000;
-    //canvas.height = window.innerHeight  * 2 / 3;
-    //canvas.width = window.innerHeight * 2 / 3;
+
+    // Resizing. 
+    // Note: HTML canvas resolution and css div resolution 
+    // are technically 2 separate properties.
+    canvas.style.height = "1024px";
+    canvas.style.width = "1024px";
+    canvas.height = 1024;
+    canvas.width = 1024; // 4MB max per layer
 
     //variables
     let painting = false;
 
+    // when you press down the cursor
     function startPosition(e) {
-        saveLayer(0, "brush");
-
         painting = true;
         draw(e);
     }
+
+    // when you release the cursor 
     function finishedPosition() {
         painting = false;
         ctx.beginPath();
+        saveLayer(0, "brush");
     }
 
+    // when the cursor moves
     function draw(e) {
-        var pos = getMousePos(canvas, e)
+        var pos = getMousePos(canvas, e);
 
-        
         if (!painting) return;
         ctx.lineWidth = brush.size;
         ctx.lineCap = "round";
@@ -115,6 +154,7 @@ function  getMousePos(canvas, evt) {
       eraser = !eraser;
   }
 
+// input management (todo: clean up and add gui input)
 document.addEventListener('keydown', function(event) {
     if (event.key == 'r') {
         brush.color = "red"
@@ -124,9 +164,10 @@ document.addEventListener('keydown', function(event) {
         brush.color = "blue"
     } if (event.key == 'e') {
         toggle_eraser();
+    } if (event.ctrlKey && event.key === 'z') {
+        undo();
+    } if (event.ctrlKey && event.key === 'y') {
+        redo();
     } 
-    if (event.ctrlKey && event.key === 'z') {
-      undo();
-    }
   });
 
---
+++ readme.md
@@ -0,0 +1,11 @@
+Developer Manual
+    Download the repo.
+    All code is in the canvas.js and index.html files.
+
+User Manual
+    Open the index.html file in your browser.
+    commands:
+        use mouse to draw (click to start drawing, release to stop)
+        press E to toggle eraser mode on/off
+        CTRL+Z and CTRL+Y to undo/redo respectively
+        press r, g, or b to select a red, green, or blue brush, respectively
