568cd19fe7e329e0481dc3b2d070cac6ef5fa457
Michael Pigeon <michaellpigeon@gmail.com>
2022-03-03 13:07:27 -0500

    started getting communication between user sessions

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,18 @@
 const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({port: 8082});
+let connected_client_sockets = [];
 
 wss.on("connection", ws => {
     console.log("new client connected!");
+    connected_client_sockets.push(ws);
 
     ws.on('message', data => {
         console.log('Client has sent us: ' + data.toString());
         ws.send("test data: " + data.toString());
+        for (let i = 0; i < connected_client_sockets.length; i++) {
+            connected_client_sockets[i].send("another person just joined!");
+        }
     });
 
     ws.on("close", () => {
